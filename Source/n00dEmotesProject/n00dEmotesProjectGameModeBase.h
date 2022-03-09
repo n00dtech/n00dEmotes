0825124bd5be0e5687a3081f1fd7cd54cf8f4200
db <jdebelec@hotmail.com>
2022-03-09 13:30:27 +1100

    Added 4.25 project

---
+++ Source/n00dEmotesProject/n00dEmotesProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "n00dEmotesProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class N00DEMOTESPROJECT_API An00dEmotesProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
